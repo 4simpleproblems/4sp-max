cca43e6b55336d70f40bc7409bcb0d2d4bebaac4
4simpleproblems-v5 <4simpleproblems@gmail.com>
2026-01-24 22:08:25 -0500

    feat: isolate games hub and implement dedicated proxy system

---
+++ GAMES/sw.js
@@ -0,0 +1,24 @@
+importScripts('/GAMES/uv/uv.bundle.js');
+importScripts('/GAMES/uv/uv.config.js');
+importScripts(__uv$config.sw || '/GAMES/uv/uv.sw.js');
+
+const uv = new UVServiceWorker();
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        (async () => {
+            if (uv.route(event)) {
+                return await uv.fetch(event);
+            }
+            return await fetch(event.request);
+        })()
+    );
+});
