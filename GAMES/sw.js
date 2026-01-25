5a81f7724104b45f5a4fd30dfbd7ad51739b094f
4simpleproblems-v5 <4simpleproblems@gmail.com>
2026-01-25 17:44:43 -0500

    Final GVC fix: Stable SW, robust native fetch/XHR overrides with corsproxy.io fallback

--- GAMES/sw.js
+++ GAMES/sw.js
@@ -14,29 +14,9 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', event => {
-
     if (uv.route(event)) {
-
         event.respondWith(uv.fetch(event));
-
     } else {
-
-        const url = new URL(event.request.url);
-
-        if (url.hostname === 'cdn.dos.zone') {
-
-            // Force proxying for dos.zone to fix CORS and use Bare transport
-
-            const proxiedUrl = self.location.origin + self.__uv$config.prefix + Ultraviolet.codec.xor.encode(event.request.url.replace(/\\/g, '/'));
-
-            event.respondWith(fetch(proxiedUrl));
-
-        } else {
-
-            event.respondWith(fetch(event.request));
-
-        }
-
+        event.respondWith(fetch(event.request));
     }
-
 });
