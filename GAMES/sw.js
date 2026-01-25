f1e2486ecd4993804fdf01c524de2aaf88ce7bb3
4simpleproblems-v5 <4simpleproblems@gmail.com>
2026-01-25 15:09:31 -0500

    Update GAMES/sw.js timestamp to force SW update

--- GAMES/sw.js
+++ GAMES/sw.js
@@ -3,6 +3,7 @@ importScripts('uv/uv.config.js');
 importScripts(__uv$config.sw || 'uv/uv.sw.js');
 
 const uv = new UVServiceWorker();
+// Updated: Sun Jan 25 2026
 
 self.addEventListener('install', (event) => {
     event.waitUntil(self.skipWaiting());
